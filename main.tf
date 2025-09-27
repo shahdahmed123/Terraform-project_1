6e5c82149e59bb428e54dacdc9b144e2a672064b
root <root@localhost.localdomain>
2025-09-27 12:30:24 +0300

    Initial commit - Terraform VPC lab

---
+++ main.tf
@@ -0,0 +1,120 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 1- VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.42.0.0/16"
+  tags = {
+    Name = "lab2-vpc"
+  }
+}
+
+# 2- Subnets
+## Public subnets
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.42.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.42.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-b"
+  }
+}
+
+## Private subnets
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.42.11.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.42.12.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private-b"
+  }
+}
+
+# 3- Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "lab2-igw"
+  }
+}
+
+# 4- Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Associate Public subnets
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+# 5- NAT Gateway (في Public-A)
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_a.id
+  tags = {
+    Name = "nat-gw"
+  }
+}
+
+# 6- Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+# Associate Private subnets
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
+
